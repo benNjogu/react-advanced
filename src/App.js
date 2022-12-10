7a2448e032d773891025f549d34b67458b032407
benNjogu <kamandemachafi@gmail.com>
2022-12-10 15:55:05 +0300

    Fetching data with hooks

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import "./App.css";
 import Counter from "./hooks/CounterClass";
 import CounterF from "./hooks/CounterFunction";
+import Users from "./hooks/Users";
 
 function App() {
-  return <CounterF />;
+  return <Users />;
 }
 
 export default App;
