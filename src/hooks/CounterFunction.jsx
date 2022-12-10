7b824d502c23b19ff6729bfce2e70445385e11e0
benNjogu <kamandemachafi@gmail.com>
2022-12-10 14:50:16 +0300

    useEffect hook

--- src/hooks/CounterFunction.jsx
+++ src/hooks/CounterFunction.jsx
@@ -1,8 +1,17 @@
-import { Fragment, useState } from "react";
+import { Fragment, useEffect, useState } from "react";
 
 const CounterF = () => {
   const [count, setCount] = useState(0);
   const [name, setName] = useState("");
+
+  useEffect(() => {
+    document.title = `${name} has clicked ${count} times`;
+
+    return () => {
+      //component will unmount
+      console.log("Clean up");
+    };
+  }, []);
 
   return (
     <Fragment>
