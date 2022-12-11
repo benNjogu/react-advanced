04b11b7f06846f90ff8f7f1f4ad2980ec0cdf388
benNjogu <kamandemachafi@gmail.com>
2022-12-11 10:16:49 +0300

    Context in class components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
+import React, { Component } from "react";
+import MoviePage from "./context/MoviePage";
+import UserContext from "./context/userContext";
 import "./App.css";
-import Counter from "./hooks/CounterClass";
-import CounterF from "./hooks/CounterFunction";
-import Users from "./hooks/Users";
 
-function App() {
-  return <Users />;
+class App extends Component {
+  state = { currentUser: { name: "Ben" } };
+  render() {
+    return (
+      <UserContext.Provider value={this.state.currentUser}>
+        <div>
+          <MoviePage />
+        </div>
+      </UserContext.Provider>
+    );
+  }
 }
 
 export default App;
